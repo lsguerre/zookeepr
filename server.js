66fcee32d689416f5f40660aac3fd7fad5e940fd
Opal <guerre5lily@gmail.com>
2022-01-04 20:49:42 -0800

    trying again..

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const PORT = process.env.PORT || 3001
 const app = express()
-const { animals } = require('./data/animals')
+const { animals } = require('./data/animals.json')
 
 
 function filterByQuery(query, animalsArray) {
