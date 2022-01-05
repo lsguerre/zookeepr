fcf4865f759ec38b4e639662a1be6e05e67462d2
Opal <guerre5lily@gmail.com>
2022-01-04 21:13:16 -0800

    commiting

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const PORT = process.env.PORT || 3001
 const app = express()
-const { animals } = require('./data/animals')
+const { animals } = require('./data/animals.json')
 
 
 function filterByQuery(query, animalsArray) {
@@ -42,7 +43,7 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-app.get('/api/animals', (req, res) => {
+app.get('/api/animals/', (req, res) => {
    let results = animals
    if (req.query) {
        results = filterByQuery(req.query, results)
@@ -50,6 +51,8 @@ app.get('/api/animals', (req, res) => {
    res.json(results)
    
 })
-app.listen(3001, () => {
-    console.log('API server now on port 3001')
-})
+
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
